df3018d57a7057430837afe9fc14b1cb8ff84522
José Consciência <jconsciencia1999@gmail.com>
2023-11-02 11:32:20 +0000

    reset of cpp 01

--- CPP01/ex03/HumanA.cpp
+++
@@ -1,9 +0,0 @@
-#include "HumanA.hpp"
-
-HumanA::HumanA(std::string name, Weapon &weapon) : _name(name), _weapon(weapon) {}
-
-HumanA::~HumanA() {}
-
-void HumanA::attack() {
-    std::cout << this->_name << " attacks with his " << this->_weapon.getType() << std::endl;
-}
