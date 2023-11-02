ba76ef91d892a467a28bc2e4798c24c393ba1a46
José Consciência <jconsciencia1999@gmail.com>
2023-11-02 14:23:24 +0000

    chore: message for destructor, plus no setter for name.

--- CPP01/ex00/Zombie.cpp
+++ CPP01/ex00/Zombie.cpp
@@ -1,7 +1,7 @@
 #include "Zombie.hpp"
 
 Zombie::Zombie (std::string name) { this->name = name; }
-Zombie::~Zombie() { return ; };
+Zombie::~Zombie() { std::cout << "Destructor for " << this->name << " was called." << std::endl; };
 
 void Zombie::announce(void){
     std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
